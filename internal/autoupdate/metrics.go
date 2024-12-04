3afb793e0da6f24633fa62ff494f96a31eab5c78
Fabian Holler <fabian.holler@simplesurance.de>
2024-12-04 09:03:16 +0100

    metrics: change prometheus namespace to "directorius"

--- internal/autoupdate/metrics.go
+++ internal/autoupdate/metrics.go
@@ -10,7 +10,7 @@ import (
 	"github.com/simplesurance/directorius/internal/logfields"
 )
 
-const metricNamespace = "goordinator_autoupdater"
+const metricNamespace = "directorius"
 
 const (
 	queueOperationsMetricName = "queue_operations_total"
